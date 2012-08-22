1c43c95b5d3133609a1d92c6894e4f81240e2b3d
EivindEE <eivind.elseth@gmail.com>
2012-08-22 13:28:57 +0200

    Added args to the script, height and width can be passed. If they are not passed a random int 1-2000 will be picked

--- RandomImage.py
+++ RandomImage.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python
 from PIL import Image
 import random
+import argparse
 
-x = 512
-y = 512
+parser = argparse.ArgumentParser(description='Creates a totally random image, optionaly with a size.')
+parser.add_argument("--width", type=int, help='the width of the image')
+parser.add_argument("--height", type=int, help='the height of the image')
+
+args = parser.parse_args()
+if args.width:
+	x = args.width
+else:
+	x = random.randint(1, 2000)
+if args.height:
+	y = args.height
+else:
+	y = random.randint(1, 2000)
 
 im = Image.new("RGB", (x,y), "green")
 
