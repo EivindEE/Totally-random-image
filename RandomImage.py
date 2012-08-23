38cd097b0a118a1e5d7e28eba0dfe9be068cea8e
EivindEE <eivind.elseth@gmail.com>
2012-08-23 15:34:32 +0200

    Changed to use RandomNumber as source of random colors. Reduced image size to 200x200 to conform to Random.org API(max 10.000 results)

--- RandomImage.py
+++ RandomImage.py
@@ -2,6 +2,7 @@
 from PIL import Image
 import random
 import argparse
+from RandomNumbers import randint
 
 parser = argparse.ArgumentParser(description='Creates a totally random image, optionaly with a size.')
 parser.add_argument("-x", "--width", type=int, help='the width of the image')
@@ -11,17 +12,19 @@ args = parser.parse_args()
 if args.width:
 	x = args.width
 else:
-	x = random.randint(1, 2000)
+	x = random.randint(1, 200)
 if args.height:
 	y = args.height
 else:
-	y = random.randint(1, 2000)
+	y = random.randint(1, 200)
 
+num = x*y
+randarray = randint(num)
 im = Image.new("RGB", (x,y), "green")
-
 for i in range(0,x):
 	for j in range(0,y):
-		im.putpixel((i,j), ((random.randint(0,255),random.randint(0,255), random.randint(0,255))))
+		pixel = randarray[(i*y)+j]
+		im.putpixel((i,j), (pixel[0],pixel[1],pixel[2]))
 
 	
 im.show()
